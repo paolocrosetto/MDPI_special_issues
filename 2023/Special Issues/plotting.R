424abfb3d3b4f75e0b471e853c6de68b97f603d2
paolocrosetto <paolo.crosetto@gmail.com>
2023-02-22 20:32:42 +0100

    plotting updated to reply to Jack Drake from THE

--- 2023/Special Issues/plotting.R
+++ 2023/Special Issues/plotting.R
@@ -69,28 +69,31 @@ yearlabels <- dfplot %>% select(label) %>% distinct() %>% pull(label)
 
 ## labels only for 2022
 dfplot <- dfplot %>% 
-  mutate(lab = if_else(year == 2023, paste0(journal, " ", n), NA_character_))
+  mutate(lab = if_else(year == 2023 | year == 2022, paste0(journal, " ", n), NA_character_))
 
 
 # label
 dfplot <- dfplot %>% 
   mutate(nd = cut(n, breaks = c(0,12,52,365,365*5,10000), labels = c("<1/month",">1/month",">1/week", ">1/day", ">5/day")))
 
+yearlabels <- unique(dfplot$label[dfplot$year !="2023"])
 # plotting
 dfplot %>% 
+  mutate(lab = if_else(n>365, lab, NA_character_)) %>% 
+  filter(year != 2023) %>% 
   mutate(nd = cut(n, breaks = c(0,12,52,365,365*5,10000), labels = c("<1/month",">1/month",">1/week", ">1/day", ">5/day"))) %>% 
   ggplot(aes(label, n, group = journal))+
   geom_line(color = "grey70", alpha = 0.4)+
-  geom_text_repel(aes(label = lab), nudge_x = 15, direction = "y", hjust = 1, segment.alpha = 0.3)+
+  geom_text_repel(aes(label = lab), nudge_x = 1, direction = "y", hjust = 1, segment.alpha = 0.3)+
   geom_quasirandom(aes(fill = nd), size = 4, pch = 21, color = "grey60", alpha = 0.7)+
   theme_ipsum()+
-  #scale_x_discrete(limits = c(yearlabels, "", ""))+
+  scale_x_discrete(limits = c(yearlabels, ""))+
   scale_fill_brewer(name = "", palette = "RdYlGn", direction = -1)+
   scale_y_continuous(breaks = c(52, 365, 365*2, 365*3, 365*4, 365*5, 365*6, 365*7, 365*8, 365*9), 
                      labels = c("1 per week", "1 per day", "2 per day", 
                                 "3 per day", "4 per day", "5 per day", 
                                 "6 per day", "7 per day", "8 per day", "9 per day"))+
-  labs(title = "Number of Special Issues at MDPI -- September 2022",
+  labs(title = "Number of Special Issues at MDPI: 2017-22",
        subtitle = "98 journals with an Impact Factor",
        caption = "code @paolocrosetto -- data scraped from MDPI website",
        y = "", x = "")+
@@ -103,9 +106,43 @@ dfplot %>%
 
 ## %>% saving the plot
 ggsave(
-  "MDPI_special_issues_2017-22_sept22_NZZ.png", 
-  plot = p,
-  width = 9/1.4, height = 14/1.4, units = "in", dpi = 200
+  "MDPI_special_issues_17_22.png", 
+  width = 14/1.1, height = 9/1.1, units = "in", dpi = 300
+)
+
+
+## including 2023
+yearlabels <- unique(dfplot$label)
+# plotting
+dfplot %>% 
+  mutate(lab = if_else(n>365, lab, NA_character_)) %>% 
+  mutate(lab = if_else(year == 2023, paste0(journal, " ", n), NA_character_)) %>% 
+  mutate(nd = cut(n, breaks = c(0,12,52,365,365*5,10000), labels = c("<1/month",">1/month",">1/week", ">1/day", ">5/day"))) %>% 
+  ggplot(aes(label, n, group = journal))+
+  geom_line(color = "grey70", alpha = 0.4)+
+  geom_text_repel(aes(label = lab), nudge_x = 1, direction = "y", hjust = 1, segment.alpha = 0.3)+
+  geom_quasirandom(aes(fill = nd), size = 4, pch = 21, color = "grey60", alpha = 0.7)+
+  theme_ipsum()+
+  scale_x_discrete(limits = c(yearlabels, ""))+
+  scale_fill_brewer(name = "", palette = "RdYlGn", direction = -1)+
+  scale_y_continuous(breaks = c(52, 365, 365*2, 365*3, 365*4, 365*5, 365*6, 365*7, 365*8, 365*9), 
+                     labels = c("1 per week", "1 per day", "2 per day", 
+                                "3 per day", "4 per day", "5 per day", 
+                                "6 per day", "7 per day", "8 per day", "9 per day"))+
+  labs(title = "Number of Special Issues at MDPI: 2017-22",
+       subtitle = "98 journals with an Impact Factor",
+       caption = "code @paolocrosetto -- data scraped from MDPI website",
+       y = "", x = "")+
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        legend.position = "bottom",
+        plot.title.position = "plot",
+        plot.background = element_rect(color = "white", fill = "white"))
+
+## %>% saving the plot
+ggsave(
+  "MDPI_special_issues_17_23.png", 
+  width = 14/1.1, height = 9/1.1, units = "in", dpi = 300
 )
 
 
